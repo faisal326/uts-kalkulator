d9295cc41fe4f2231d030710781ee28747d4fe50
hp <hp@hp-PC>
2016-11-14 12:56:14 +0700

    11140349-Saya telah membuat kode agar bilangan bisa dijumlahkan

--- src/kalkulatorbeta/kalkulator.java
+++ src/kalkulatorbeta/kalkulator.java
@@ -61,7 +61,16 @@ int jumlah, bil1, bil2, pilih;
         angka +=".";
         txttampil.setText(angka);
     }
-    
+    void hasil(){
+        switch (pilih){
+            case 1 : 
+                bil2 = Integer.parseInt(angka);
+                jumlah = bil1+bil2;
+                angka = Integer.toString(jumlah)
+                txttampil.setText(angka);
+                break;
+        }
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
